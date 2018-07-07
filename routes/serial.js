0e8d373165ffe0facbe138875cc36eae3f023ecc
Kerry <mrkerrywashington@icloud.com>
2018-07-07 10:39:40 -0700

    tyring something new

--- routes/serial.js
+++ routes/serial.js
@@ -2,7 +2,11 @@
 var express = require('express');
 var router = express.Router();
 var util = require("util");
-var ax25 = require('th-d72-ax25');
+ //var ax25 = require('th-d72-ax25');
+
+
+var SerialPort	= require("serialport");
+const Readline = require('@serialport/parser-readline')
 
 
 var devicePath = '/dev/ttyUSB0';
@@ -14,6 +18,23 @@ if (osvar == 'darwin') {
 }else{ 
 	devicePath = '/dev/ttyUSB0';
 }
+
+
+
+var port = new SerialPort(devicePath, function (err) {
+	if (err) {
+	  return console.log('Error: ', err.message);
+	}
+  });
+   
+  port.write('main screen turn on', function(err) {
+	if (err) {
+	  return console.log('Error on write: ', err.message);
+	}
+	console.log('message written');
+  });
+
+
 
 
 router.get('/', function (req,res) { 
@@ -31,59 +52,7 @@ router.get('/', function (req,res) {
 //     res.send('POST Handler for the /terminal endppint');
 // });
 
- 
-var tnc = new ax25.kissTNC(
-    {	'serialPort' : devicePath,
-        'baudRate' : 9600
-    }
-);
- 
-var beacon = function() {
-    var packet = new ax25.Packet(
-        {	sourceCallsign : "MYCALL",
-            destinationCallsign : "BEACON",
-            type : ax25.U_FRAME_UI,
-            infoString : "Hello world!"
-        }
-    );
-    var frame = packet.assemble();
-    tnc.send(frame);
-    console.log("Beacon sent.");
-}
- 
-tnc.on(
-    "error",
-    function(err) {
-        console.log(err);
-    }
-);
- 
-tnc.on(
-    "opened",
-    function() {
-        console.log("TNC opened on " + tnc.serialPort + " at " + tnc.baudRate);
-        setInterval(beacon, 30000); // Beacon every 30 seconds - excessive!
-    }
-);
- 
-tnc.on(
-    "frame",
-    function(frame) {
-        var packet = new ax25.Packet({ 'frame' : frame });
-        console.log(
-            util.format(
-                "Packet seen from %s-%s to %s-%s.",
-                packet.sourceCallsign,
-                packet.sourceSSID,
-                packet.destinationCallsign,
-                packet.destinationSSID
-            )
-        );
-        if(packet.infoString != "")
-            console.log(packet.infoString);
-    }
-);
-
+  
 
 
 
