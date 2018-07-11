9980aa79c2f00ceca97bf4db91638e541a11021a
kcw-grunt <mrkerrywashington@me.com>
2018-07-10 17:53:21 -0700

    rules.d and works with single char!!!!!!!

--- routes/serial.js
+++ routes/serial.js
@@ -3,43 +3,30 @@ var express = require('express');
 var router = express.Router(); 
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
-var util = require("util"), repl = require("repl");
+//const Readline = SerialPort.parsers.Readline;  
+var devicePath = '/dev/KENWOOD_TH-D72A';
+var dataLine = [];
+// var osvar = process.platform;
+// console.log(osvar);
+// if (osvar == 'darwin') {
+// 	console.log("Using Mac OS");
+//     devicePath = '/dev/tty.SLAB_USBtoUART';
+// }else{ 
+// 	devicePath = '/dev/ttyUSB2';
+// }
+    
+var port = new SerialPort(devicePath, {
+  baudRate: 9600,
+  parity: 'none',
+  stopBits: 1,
+  dataBits: 8,
+  flowControl: false
+}, function(err) {
+  if (err){
+    console.log('error: ', err.message);
+    port.close();
+}});
 
-serialport.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-  });
-});
-
-  
-
-var devicePath = '/dev/ttyUSB2';
-
-var osvar = process.platform;
-console.log(osvar);
-if (osvar == 'darwin') {
-	console.log("Using Mac OS");
-    devicePath = '/dev/tty.SLAB_USBtoUART';
-}else{ 
-	devicePath = '/dev/ttyUSB2';
-}
- 
-  ///,
-  //parser: SerialPort.parsers.raw
-var port = new SerialPort( devicePath, { // change path
-  baudrate: 9600
-, function ( err ) {
-  if ( err ) {
-    console.error('error opening serial port:' , err);
-  }
-}
-});
-
-port.on('data', function(data) {
-  console.log(data)
-});
  
 port.write('KISS ON\r\n', function(err) {
   console.log('KISS ON Turned on');
@@ -47,6 +34,16 @@ port.write('KISS ON\r\n', function(err) {
 
 port.write('RESTART\r\n', function(err) {
   console.log('Restarted');
+});
+
+port.on('data', function(data) {
+
+  // if(data =='0D 0A') {
+  //   console.log('Date line:'+ dataline)
+  // } else {
+  //   dataLine.push(data);
+  // }
+  console.log('Data:', data);
 });
 
 port.on('open', function() {
